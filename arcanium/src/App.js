4b2f35601ede2e85872cd11ca4327d5be825c58f
Owen-kc <ownie44@gmail.com>
2023-10-19 16:05:31 +0100

    Testing simple API calls and searching

--- arcanium/src/App.js
+++ arcanium/src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [dndData, setDndData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const apiUrl = `https://api.open5e.com/monsters/?name=${searchTerm}`;
+    
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setDndData(data.results || []);
+      })
+      .catch((error) => console.error('Error fetching D&D data:', error));
+  }, [searchTerm]);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>D&D Monster Search</h1>
+        
+        <div>
+          <input
+            type="text"
+            placeholder="Search for a D&D monster..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </div>
+
+        <div>
+          {dndData.length > 0 ? (
+            <div>
+              <h3>Monster Data</h3>
+              <ul>
+                {dndData.map((item) => (
+                  <li key={item.index}>{item.name}</li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <p>No data available.</p>
+          )}
+        </div>
       </header>
     </div>
   );
